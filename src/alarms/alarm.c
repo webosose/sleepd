f428645f08607ef0e0580d6258378c3d4b3a570b
Sandeep Arjun <sandeep.arjun@lge.com>
2020-05-19 14:03:20 +0530

    Fix memory leaks
    :Release Notes:
    Fix memory leaks found
    
    :Detailed Notes:
    Fix memory leaks due to missing free calls for xmlChar
    objects allocated on heap
    
    :Testing Performed:
    Build Successful. See CCC for test results.
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-104085]: Test External HDMI during 240h, memory
                   leak is observed from /usr/sbin/sleepd
    
    Change-Id: I7f8f1b784bb684da6ada8d331c17e0a1a9b826d1

--- src/alarms/alarm.c
+++ src/alarms/alarm.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2018 LG Electronics, Inc.
+// Copyright (c) 2011-2020 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -804,6 +804,7 @@ alarm_read_db(void)
 
     if (!cur)
     {
+        xmlFreeDoc(db);
         return;
     }
 
@@ -856,7 +857,10 @@ alarm_read_db(void)
             }
 
 clean_round:
+            xmlFree(id);
+            xmlFree(key);
             xmlFree(expiry);
+            xmlFree(calendar);
             xmlFree(service);
             xmlFree(app);
         }
